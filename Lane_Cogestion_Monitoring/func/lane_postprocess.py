f70773797237f949290cba2badaef25210ae4119
Sweta <e1350675@u.nus.edu>
2025-04-15 10:46:02 +0800

    post proc file for pipeline test

--- Lane_Cogestion_Monitoring/func/lane_postprocess.py
+++ Lane_Cogestion_Monitoring/func/lane_postprocess.py
@@ -1,49 +1,50 @@
+# import hailo
+from gsthailo import VideoFrame
+# from gi.repository import Gst
 
-
-def process_lane_detections(lane_detections, ld_frame_width, ld_frame_height, model_type):
+# may be needs to be added in the LD callback, not in the gstsreamer.
+def process_lane_detections(video_frame: VideoFrame, model_type):
     """
     Process lane detections to filter and format the data.
 
     Args:
-        lane_detections (list): List of lane detection results.
-        ld_frame_width (int): Width of the frame.
-        ld_frame_height (int): Height of the frame.
+        video_frame (VideoFrame): The video frame containing lane detections.
         model_type (str): Type of the lane detection model used.
 
     Returns:
         list: Processed lane detections.
     """
-    processed_detections = []
+    roi = video_frame.roi
+    tensrs = roi.get_tensors()
+    print("Checking the LD post processing pipeline")
 
     match model_type:
-        case "UFLD":
-            # UFLD processing - detecttion results are in the format [(x, y), confidence scores, may be Lane IDs]
-            for detection in lane_detections:
-                # Filter out detections with low confidence
-                if detection['confidence'] < 0.5:
-                    continue
+        case "anchor":
+            # anchor based model - UFLDv2 processing - detection results are in the format [(x, y), confidence scores, may be Lane IDs]
 
-                # Normalize coordinates to frame size
-                detection['x1'] = int(detection['x1'] * ld_frame_width)
-                detection['y1'] = int(detection['y1'] * ld_frame_height)
-                detection['x2'] = int(detection['x2'] * ld_frame_width)
-                detection['y2'] = int(detection['y2'] * ld_frame_height)
+            for tensor in tensrs:
+                print(tensor)
+                print(tensor.name())         
+            
+        # case "segmentation":
+        #     # segmentation based model - LaneNet, more accuracte SCNN or ENet-SAD processing
+        #     for tensor in tensrs:
+        #         print(tensor)
+        #         print(tensor.name())
+                    
 
-                processed_detections.append(detection)
 
-        case "Hailo":
-            # Hailo processing
-            for detection in lane_detections:
-                # Filter out detections with low confidence
-                if detection['confidence'] < 0.5:
-                    continue
+        # case "keypoint":
+        #     # Keypoint based model - FOLOLane processing
+        #     for tensor in tensrs:
+        #         print(tensor)
+        #         print(tensor.name()) 
 
-                # Normalize coordinates to frame size
-                detection['x1'] = int(detection['x1'] * ld_frame_width)
-                detection['y1'] = int(detection['y1'] * ld_frame_height)
-                detection['x2'] = int(detection['x2'] * ld_frame_width)
-                detection['y2'] = int(detection['y2'] * ld_frame_height)
 
-                processed_detections.append(detection)
+        # case "parameter":
+        #     # Parameter based model - example - PolyLANENET processing
+        #     for tensor in tensrs:
+        #         print(tensor)
+        #         print(tensor.name())
 
-    return processed_detections
+                
